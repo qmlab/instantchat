19d14bd5b8dc346e152391700eddcd148bf00fbd
qmlab <mainchin@msn.com>
2015-02-17 11:02:19 -0800

    add configs and compression

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 // Setup basic express server
 var express = require('express')
 , app = express()
-, https = require('https')
 , chatserver = require('./chatserver.js')
 , path = require('path')
 , i18n = require('i18next')
 , fs = require('fs')
+, nconf = require('nconf')
+, compress = require('compression')
 
-var port = 3000
+// First consider commandline arguments and environment variables, respectively.
+nconf.argv().env();
+
+// Whether this is debug or release
+var isDebug = false
+
+if (nconf.get('debug')) {
+  console.log('debug mode')
+  isDebug = true
+}
+
+if (!isDebug) {
+  // Provide configs for release
+  nconf.file({ file: 'config.release.json' });
+}
+else {
+  // Provide configs for release
+  nconf.file({ file: 'config.debug.json' });
+}
+
+var port = nconf.get('port')
 
 // For rendering views
 app.set('views', __dirname + '/public')
@@ -30,14 +51,22 @@ i18n.init(function(t) {
     res.render('chat.ejs')
   })
 
-  var privateKey  = fs.readFileSync('certs/talkyet.key', 'utf8')
-  var certificate = fs.readFileSync('certs/talkyet.cer', 'utf8')
-  var credentials = {key: privateKey, cert: certificate}
-  var server = https.createServer(credentials, app)
-  server.listen(port, function () {
-    console.log('Server listening at port %d', port);
-  })
+  var server
+  if (!isDebug) {
+    var privateKey  = fs.readFileSync('certs/talkyet.key', 'utf8')
+    var certificate = fs.readFileSync('certs/talkyet.cer', 'utf8')
+    var credentials = {key: privateKey, cert: certificate}
+    server = require('https').createServer(credentials, app)
+    server.listen(port, function () {
+      console.log('Server listening at port %d', port)
+    })
+  }
+  else {
+    server = require('http').createServer(app)
+    server.listen(port, function() {
+      console.log('Debug: server listening at port %d', port)
+    })
+  }
 
-  // Server
   chatserver.start(server)
 })
