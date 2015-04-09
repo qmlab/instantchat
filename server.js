78cb49ea33f315b24902af82305f991db42b5bae
qmlab <mainchin@msn.com>
2015-04-08 22:43:26 -0700

    use configured port

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ else {
   nconf.file({ file: 'config.debug.json' });
 }
 
-//var port = nconf.get('port')
-var port = process.env.PORT || 3000;
+var port = nconf.get('port')
+//var port = process.env.PORT || 3000;
 
 // For rendering views
 app.set('views', __dirname + '/public')
